c53655a4a90f4b726dbaebb6e4e0cdcd1b637be7
peerapong <flamerock0210@gmail.com>
2018-04-29 23:35:30 +0700

    START C# WHIT BORNTODEV

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int x = 10;
+            int y = 20;
+            int z = x + y;*/
+            Console.WriteLine("HELLO FRAME" + Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
