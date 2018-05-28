7e1b2d4c6b1c5fa63ca0e12e1f04c26c95dcc25b
peerapong <flamerock0210@gmail.com>
2018-05-29 00:49:04 +0700

    เก็บข้อมูลลูกค้า แบบ งง ๆ -*-

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -46,17 +46,43 @@ namespace ConsoleApp1
             customers[4, 1] = Console.ReadLine();
             Console.WriteLine("Enter your mobile");
             customers[4, 2] = Console.ReadLine();
-            Console.WriteLine("");
-            Console.WriteLine("Report>>");
-            foreach (string data in customers)
-            {
-                
-                Console.WriteLine(data);
-            }
-            //Console.WriteLine(customers[0, 0]);
-            //Console.WriteLine(customers[0, 1]);
-            //Console.WriteLine(customers[0, 2]);
-            //Console.WriteLine(customers[0, 0]);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Data of customer ");
+
+            Console.Write("no.1 ");
+            Console.Write(customers[0, 0]);
+            Console.Write(" address ");
+            Console.Write(customers[0, 1]);
+            Console.Write(" mobile ");
+            Console.WriteLine(customers[0, 2]);
+
+            Console.Write("no.2 ");
+            Console.Write(customers[1, 0]);
+            Console.Write(" address ");
+            Console.Write(customers[1, 1]);
+            Console.Write(" mobile ");
+            Console.WriteLine(customers[1, 2]);
+
+            Console.Write("no.3 ");
+            Console.Write(customers[2, 0]);
+            Console.Write(" address ");
+            Console.Write(customers[2, 1]);
+            Console.Write(" mobile ");
+            Console.WriteLine(customers[2, 2]);
+
+            Console.Write("no.4 ");
+            Console.Write(customers[3, 0]);
+            Console.Write(" address ");
+            Console.Write(customers[3, 1]);
+            Console.Write(" mobile ");
+            Console.WriteLine(customers[3, 2]);
+
+            Console.Write("no.5 ");
+            Console.Write(customers[4, 0]);
+            Console.Write(" address ");
+            Console.Write(customers[4, 1]);
+            Console.Write(" mobile ");
+            Console.Write(customers[4, 2]);
             Console.ReadLine();
         }
     }
