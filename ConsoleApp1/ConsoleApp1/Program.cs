ce14a5c6c5b8f5b3dbf0218b2a6abee301a0447e
peerapong <flamerock0210@gmail.com>
2018-05-28 21:46:03 +0700

    การบ้าน array

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,10 +10,54 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string[,] customers = { { "Hello", "world" },{"s","-" } };
-            string[][] users = new string [ 5 ][];
-            users[0] = new string[3];
-            users[1] = new string[4];
+            string[,] customers = {{ "name","address","mobile" }, { "name", "address", "mobile" }, { "name", "address", "mobile" }, { "name", "address", "mobile" }, { "name", "address", "mobile" } };
+
+            Console.WriteLine("Enter your name");
+            customers[0, 0] = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            customers[0, 1] = Console.ReadLine();
+            Console.WriteLine("Enter your mobile");
+            customers[0, 2] = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Enter your name");
+            customers[1, 0] = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            customers[1, 1] = Console.ReadLine();
+            Console.WriteLine("Enter your mobile");
+            customers[1, 2] = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Enter your name");
+            customers[2, 0] = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            customers[2, 1] = Console.ReadLine();
+            Console.WriteLine("Enter your mobile");
+            customers[2, 2] = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Enter your name");
+            customers[3, 0] = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            customers[3, 1] = Console.ReadLine();
+            Console.WriteLine("Enter your mobile");
+            customers[3, 2] = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Enter your name");
+            customers[4, 0] = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            customers[4, 1] = Console.ReadLine();
+            Console.WriteLine("Enter your mobile");
+            customers[4, 2] = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Report>>");
+            foreach (string data in customers)
+            {
+                
+                Console.WriteLine(data);
+            }
+            //Console.WriteLine(customers[0, 0]);
+            //Console.WriteLine(customers[0, 1]);
+            //Console.WriteLine(customers[0, 2]);
+            //Console.WriteLine(customers[0, 0]);
+            Console.ReadLine();
         }
     }
 }
